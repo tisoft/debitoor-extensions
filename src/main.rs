9b106a5e3086120f16fb43221825035c4d9ff3f9
Markus Heberling <markus@tisoft.de>
2016-12-30 17:50:28 +0100

    connect to the debitoor API and parse the response into a json structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+extern crate serde_json;
+#[macro_use]
+extern crate hyper;
+
+use std::env;
+use hyper::Client;
+use hyper::header::Connection;
+use serde_json::Value;
+use std::vec::Vec;
+
+header! { (XToken, "x-token") => [String] }
+
 fn main() {
-    println!("Hello, world!");
+    let token = env::args().skip(1).next().unwrap();
+
+    let client = Client::new();
+
+    println!("send request for token {}", token);
+    let res = client.
+        get("https://api.debitoor.com/api/expenses/v3").
+        //if we keep the connection open the parsing will wait for a minute in between for a timeout
+        //don't know why this is, so just disable keep alive for now
+        header(Connection::close()).
+        //the access token to authenticate with
+        header(XToken(token.to_owned())).
+        send().unwrap();
+    assert_eq!(res.status, hyper::Ok);
+
+    println!("create parser");
+
+    let x: Value = serde_json::from_reader(res).unwrap();
+
+    println!("printing value");
+
+    println!("{:?}", x);
+
+    println!("all done");
 }
