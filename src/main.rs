8f83af756c32c7c425255beac2c7fd8b312d87ba
Markus Heberling <markus@tisoft.de>
2017-01-01 13:26:52 +0100

    listen on port 8080 and output result there

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate chrono;
 
 use std::env;
 use hyper::Client;
+use hyper::Server;
 use hyper::header::Connection;
 use std::vec::Vec;
 
@@ -14,33 +15,47 @@ header! { (XToken, "x-token") => [String] }
 include!(concat!(env!("OUT_DIR"), "/serde_types.rs"));
 
 fn main() {
-    let token = env::args().skip(1).next().unwrap();
+    let port = env::var("DEBITOOR_EXTENSIONS_PORT").or(Ok::<String, std::env::VarError>("8080".to_string())).unwrap().parse::<i32>().unwrap();
 
-    let client = Client::new();
+    fn hello(serverReq: hyper::server::Request, serverRes: hyper::server::Response) {
+        //println!("Incoming request {:?}", req);
 
-    println!("send request for token {}", token);
-    let res = client.
-        get("https://api.debitoor.com/api/expenses/v3").
-        //if we keep the connection open the parsing will wait for a minute in between for a timeout
-        //don't know why this is, so just disable keep alive for now
-        header(Connection::close()).
-        //the access token to authenticate with
-        header(XToken(token.to_owned())).
-        send().unwrap();
-    assert_eq!(res.status, hyper::Ok);
+        let token = env::args().skip(1).next().unwrap();
 
-    println!("create parser");
+        let client = Client::new();
 
-    let expenses: Vec<Expense> = serde_json::from_reader(res).unwrap();
+        println!("send request for token {}", token);
+        let res = client.
+            get("https://api.debitoor.com/api/expenses/v3").
+            //if we keep the connection open the parsing will wait for a minute in between for a timeout
+            //don't know why this is, so just disable keep alive for now
+            header(Connection::close()).
+            //the access token to authenticate with
+            header(XToken(token.to_owned())).
+            send().unwrap();
+        assert_eq!(res.status, hyper::Ok);
 
-    println!("printing value");
+        println!("create parser");
 
+        let expenses: Vec<Expense> = serde_json::from_reader(res).unwrap();
 
-    for expense in expenses {
-        for line in expense.lines.iter().filter(|line| line.category_type == "asset") {
-            println!("{:?}", line);
+        println!("printing value");
+
+        let mut asset_string = "".to_string();
+
+        for expense in expenses {
+            for line in expense.lines.iter().filter(|line| line.category_type == "asset") {
+                println!("{:?}", line);
+                asset_string = asset_string + &*format!("{:?}\n", line);
+            }
         }
+
+        println!("Sending response");
+        serverRes.send(asset_string.as_bytes()).unwrap();
+
+        println!("all done");
     }
 
-    println!("all done");
+    println!("listening on {:?}", port);
+    Server::http(&*format!("0.0.0.0:{}", port)).unwrap().handle(hello).unwrap();
 }
